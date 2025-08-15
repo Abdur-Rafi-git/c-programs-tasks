3756d146562df501c593ff3465b2054595e49077
Abdur-Rafi-git <artaseenbubt@gmail.com>
2025-08-15 19:38:14 +0600

    Update lengthconv.c

--- length converter/lengthconv.c
+++ length converter/lengthconv.c
@@ -5,12 +5,7 @@ int main()
     printf("input length in cm to convert =");
     scanf:("%lf", &l);
 
-    /*
-    Write a C program to input length in centimeter and convert it to 
-    meter and kilometer. How to convert length from centimeter to meter and kilometer
-     in C programming. Length conversion program in C from centimeter to meter and centimeter to kilometer.
-    */
-
+   
     double km=l/100000;
     printf("\nthe km is= %lf", km);
 
